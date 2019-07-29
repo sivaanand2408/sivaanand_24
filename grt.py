3597f62a5fb485ac64d7802be9894266be145918
sivaanand2408 <53076455+sivaanand2408@users.noreply.github.com>
2019-07-29 21:57:41 +0530

    Update grt.py

--- grt.py
+++ grt.py
@@ -1,7 +1,7 @@
 l1, l2, l3 = input().split() 
-if l1 > l2 and l1 > l3:
+if l1 >= l2 and l1 >= l3:
     print(l1)
-elif l2 > l1 and l2 > l3:
+elif l2 >= l1 and l2 >= l3:
     print(l2)
 else:
     print(l3)
